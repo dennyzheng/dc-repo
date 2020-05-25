d491cbf1ff31e318bba16ec4340fabecf27cd626
zcf <78002854@qq.com>
2020-05-25 16:08:02 +0800

    添加TestGitController、applicationContext.xml（配置调度器）两个文件

---
+++ dc/src/main/java/com/hx/dc/controller/TestGitController.java
@@ -0,0 +1,13 @@
+package com.hx.dc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by zcf on 2020/5/25.
+ */
+@Controller
+@RequestMapping("testGit")
+public class TestGitController {
+
+}
